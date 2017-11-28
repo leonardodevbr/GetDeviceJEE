c88b9fec943815c3d8c9754db72c60fed88db478
Leonardo Oliveira <adsleonardo.o@gmail.com>
2017-11-28 08:59:47 -0200

    Inicial

---
+++ src/br/com/getdevice/servlet/OiMundo.java
@@ -0,0 +1,34 @@
+package br.com.getdevice.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value="/oi", initParams = {@WebInitParam(name = "nome", value = "Leonardo"), @WebInitParam(name = "idade", value = "21")})
+public class OiMundo extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		ServletConfig config = getServletConfig();
+		String nome = config.getInitParameter("nome");
+		String idade = config.getInitParameter("idade");
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Primeira Servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("Nome: "+nome+"<br>Idade: "+idade+" anos");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
